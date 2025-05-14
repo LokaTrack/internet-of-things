ac53b78466435351616457e7dfc8711b5db48c04
Quileon <121283381+quileon@users.noreply.github.com>
2025-05-14 10:32:11 +0700

    Set NTP config

---
+++ include/ntp_config.h
@@ -0,0 +1,9 @@
+#if !defined(NTP_CONFIG_H)
+#define NTP_CONFIG_H
+
+// NTP server settings
+#define NTP_SERVER "time.nist.gov" // NTP server address
+#define GMT_OFFSET 0               // GMT offset in seconds
+#define DST_OFFSET 3600            // Daylight Saving Time offset in seconds
+
+#endif // NTP_CONFIG_H
