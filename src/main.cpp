38b7627ebb55c4839067548140abff3265f3154b
Quillzyy <121283381+Quillzyy@users.noreply.github.com>
2025-05-14 11:53:45 +0700

    Timestamp configured with ISO format

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,7 @@
 #include "mqtt_config.h"
 #include "pins_config.h"
 #include "wifi_config.h"
+#include "ntp_config.h"
 
 #include <Arduino.h>
 #include <TinyGPSPlus.h>
@@ -14,11 +15,15 @@
 #endif
 #include <PubSubClient.h>
 #include <ArduinoJson.h>
-#include <ChaCha20.h> // Include the encryption header
+#include <ChaCha20.h>  // Include the encryption header
+#include <ESP32Time.h> // Include the RTC library
 
 // GPS Setup
 TinyGPSPlus gps;
 HardwareSerial gpsSerial(2); // Use Serial2 as `gpsSerial` for GPS
+
+// RTC Setup
+ESP32Time rtc(GMT_OFFSET);
 
 #ifndef USE_WIFI_CONNECTION
 // GSM Modem Setup
@@ -48,8 +53,137 @@ uint32_t lastPublishTime = 0;
 void publishGpsData();
 #ifndef USE_WIFI_CONNECTION
 void connectGprs();
+void syncNtpTime();
 #endif
 void connectWifi();
+
+// NTP Time sync function for SIM800L
+#ifndef USE_WIFI_CONNECTION
+void syncNtpTime()
+{
+  Serial.print("Synchronizing time with NTP server: ");
+  Serial.print(NTP_SERVER);
+  Serial.print("...");
+
+  // First make sure the GPRS is connected
+  if (!modem.isGprsConnected())
+  {
+    Serial.println("GPRS not connected. Cannot sync time.");
+    return;
+  }
+
+  // SIM800L specific AT commands for NTP sync
+  gsmAtSerial.println("AT+CNTPCID=1"); // Set PDP context for NTP
+  delay(1000);
+
+  // Clear any pending responses
+  while (gsmAtSerial.available())
+  {
+    gsmAtSerial.read();
+  }
+
+  // Set NTP server address and time zone
+  String ntpCommand = "AT+CNTP=\"" + String(NTP_SERVER) + "\"," + String(GMT_OFFSET / 3600);
+  gsmAtSerial.println(ntpCommand);
+  delay(1000);
+
+  // Read response
+  String response = "";
+  unsigned long timeout = millis() + 5000; // 5 second timeout
+  while (millis() < timeout)
+  {
+    if (gsmAtSerial.available())
+    {
+      char c = gsmAtSerial.read();
+      response += c;
+      if (response.indexOf("OK") != -1)
+      {
+        break;
+      }
+    }
+  }
+
+  if (response.indexOf("OK") == -1)
+  {
+    Serial.println("Failed to set NTP server!");
+    return;
+  }
+
+  // Request time synchronization
+  gsmAtSerial.println("AT+CNTP");
+  delay(1000);
+
+  // Read response for +CNTP: 1 which indicates success
+  response = "";
+  timeout = millis() + 10000; // 10 second timeout
+  while (millis() < timeout)
+  {
+    if (gsmAtSerial.available())
+    {
+      char c = gsmAtSerial.read();
+      response += c;
+      if (response.indexOf("+CNTP: 1") != -1)
+      {
+        break;
+      }
+    }
+  }
+
+  if (response.indexOf("+CNTP: 1") == -1)
+  {
+    Serial.println("Failed to sync time!");
+    return;
+  }
+
+  // Get the network time
+  gsmAtSerial.println("AT+CCLK?");
+  delay(1000);
+
+  // Read response with format +CCLK: "YY/MM/DD,HH:MM:SS±ZZ"
+  response = "";
+  timeout = millis() + 5000;
+  while (millis() < timeout)
+  {
+    if (gsmAtSerial.available())
+    {
+      char c = gsmAtSerial.read();
+      response += c;
+      if (response.indexOf("+CCLK:") != -1 && response.indexOf("OK") != -1)
+      {
+        break;
+      }
+    }
+  }
+
+  // Parse the time from response
+  int cclkIndex = response.indexOf("+CCLK: \"");
+  if (cclkIndex != -1)
+  {
+    // Extract the time string
+    String timeStr = response.substring(cclkIndex + 8, response.indexOf("\"", cclkIndex + 8));
+
+    // Parse the time components (YY/MM/DD,HH:MM:SS±ZZ)
+    int year = 2000 + timeStr.substring(0, 2).toInt(); // Convert YY to YYYY
+    int month = timeStr.substring(3, 5).toInt();
+    int day = timeStr.substring(6, 8).toInt();
+    int hour = timeStr.substring(9, 11).toInt();
+    int minute = timeStr.substring(12, 14).toInt();
+    int second = timeStr.substring(15, 17).toInt();
+
+    // Set the ESP32 RTC
+    rtc.setTime(second, minute, hour, day, month, year);
+
+    Serial.println("Success!");
+    Serial.print("Current time: ");
+    Serial.print(rtc.getTime("%Y-%m-%d %H:%M:%S"));
+    Serial.println(" UTC");
+  }
+  else
+  {
+    Serial.println("Failed to get time!");
+  }
+}
+#endif
 
 void setup()
 {
@@ -94,6 +228,10 @@ void setup()
   Serial.println("Success!");
 
   connectGprs(); // Connect to GPRS
+
+  // Synchronize time with NTP server
+  Serial.println("Synchronizing time with NTP server...");
+  syncNtpTime();
 #else
   connectWifi(); // Connect to WiFi
 #endif
@@ -220,6 +358,18 @@ void publishGpsData()
 
   // Add device ID using MQTT_CLIENT_ID from config.h
   doc["id"] = MQTT_CLIENT_ID;
+
+  // Add timestamp with ISO 8601 format including milliseconds
+  // Format: YYYY-MM-DDThh:mm:ss.sssZ for UTC
+  String isoTimestamp = rtc.getTime("%Y-%m-%dT%H:%M:%S");
+  // Add milliseconds (approximate)
+  unsigned long msInThisSecond = millis() % 1000;
+  char msStr[5];
+  sprintf(msStr, ".%03lu", msInThisSecond);
+  isoTimestamp += msStr;
+  isoTimestamp += "Z"; // 'Z' indicates UTC timezone
+
+  doc["timestamp"] = isoTimestamp;
 
   // Add GPS data
   if (gps.location.isValid())
